f7e5f44303a978ffed2866ee0de6bd6575a430d4
Sowwiezn <businka16.04.2007@gmail.com>
2023-08-30 16:16:05 +0300

    v19

--- js.js
+++ js.js
@@ -1,16 +1,15 @@
 let gridProduct = document.getElementById("grid-products")
-let cartProduct = document.getElementById("cart-products")
-let array = []
+let products = []
 let xhr = new XMLHttpRequest()
 let url = 'https://my-json-server.typicode.com/Sowwiezn/marketplace'
 
 xhr.open('GET', url +'/products')
 xhr.responseType = 'json'
 xhr.onload = function(){
-    let products = xhr.response
+    products = xhr.response
     gridProduct.innerHTML = null
     products.forEach(p => {
-      array.push(p)
+      products.push(p)
       let pElem = document.createElement('div')
       pElem.classList.add('product')
       pElem.innerHTML = `
@@ -19,17 +18,48 @@ xhr.onload = function(){
         <p class="product-price"><b>Price: </b>${p.price}</p>
         <p class="product-description"><b>Description: </b>${p.description}</p>
         <a href="userP.html?id=${p.author_id}">Seller profile</a>
-        <button>Buy</button>
+        <button onclick="addProductToCart(${p.id})">Buy</button>
       `
       gridProduct.append(pElem) 
     });
 }
 xhr.send()
 
+let cartProduct = document.getElementById("cart-products")
 let cart = [];
 if(localStorage.getItem('cart')) {
     cart = JSON.parse(localStorage.getItem('cart'));
     drawCartProducts();
+}
+
+function addProductToCart(id){
+  let product = products.find(function(p){
+    return p.id == id
+  })
+  cart.push(product)
+  drawCartProducts()
+  localStorage.setItem("cart", JSON.stringify(cart))
+  document.getElementById("cart-bttn").classList.add("active")
+  setTimeout(function(){
+    document.getElementById("cart-bttn").classList.remove("active")
+  }, 500)
+}
+
+function drawCartProducts(){
+  if(cart.length === 0) return cartProduct.innerHTML = 'Cart is empty';
+    cartProduct.innerHTML = null;
+    let sum = 0;
+    cart.forEach(function(p){
+             cartProduct.innerHTML += `
+             <p><img src="${p.img}"> ${p.name} |${p.price}$</p>
+             <hr>
+            `
+        sum += p.price;
+    });
+    cartProduct.innerHTML += `
+        <p>Total Price: ${sum}$</p>
+        <button onclick="buyAll()">Buy All</button>
+        `
 }
 
 function cartOpen(){
